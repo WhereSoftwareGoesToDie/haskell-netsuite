383fdd5c067e49ba21b701819ee15e177b7c4f60
Geoffrey Roberts <groberts@anchor.net.au>
2014-10-24 16:37:22 +1100

    Wrap Monoid Value inside ConcatValue to prevent leakage

--- src/Netsuite/Types/Compile.hs
+++ src/Netsuite/Types/Compile.hs
@@ -12,10 +12,12 @@ import qualified Data.Vector as Vector
 import Netsuite.Helpers
 import Netsuite.Restlet.Response
 
+newtype ConcatValue = ConcatValue { unConcatValue :: Value }
+
 -- | Parse all objects and mconcat them.
 responseToAeson :: RestletResponse -> Value
 responseToAeson (RestletErrorResp _) = error "Netsuite.Types.Compile.responseToAeson: Received a RestletErrorResp when we were expecting a RestletOk."
-responseToAeson (RestletOk strings)  = mconcat $ map singleResponseToAeson strings
+responseToAeson (RestletOk strings)  = unConcatValue . mconcat . map (ConcatValue . singleResponseToAeson) $ strings
   where
     -- | Parse a single object.
     singleResponseToAeson x =
@@ -25,10 +27,10 @@ responseToAeson (RestletOk strings)  = mconcat $ map singleResponseToAeson strin
     maybeVal = decode . BSL.fromStrict
 
 -- | Define a monoid instance so we can get mconcat for free.
-instance Monoid Value where
-    mempty  = object []
+instance Monoid ConcatValue where
+    mempty  = ConcatValue (object [])
     mappend = concatResults
       where
         -- | Append arrays, or take the first object of any other kind.
-        concatResults (Array x) (Array y)   = Array $ (Vector.++) x y
+        concatResults (ConcatValue (Array x)) (ConcatValue (Array y)) = ConcatValue . Array $ (Vector.++) x y
         concatResults x _                   = x
